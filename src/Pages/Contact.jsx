105ce0a849e2633b1300f08f5d3c34d282071d8f
lana duval <lanaduval09@gmail.com>
2022-12-30 11:34:52 +0100

    made CSS for contact form

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -22,26 +22,29 @@ export default function Contact() {
 
     return (
         <div>
-    <h1>Contact form</h1>
+        <div className="contactForm">
+    <h1 >Pour collaborer, <br/> envoyez-moi un message ici :</h1>
+
+
+
+<form className='contactFormLabel' ref={form} onSubmit={sendEmail}>
+      <label >Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input id="sendButton"type="submit" value="Send" />
+    </form>
+    </div>
 
 <FontAwesomeIcon className='fab fa-react fa-beat  fa-2x' icon={faBell} />
 <FontAwesomeIcon className='fab fa-react fa-fade  fa-2x' icon={faEnvelope} />
 <FontAwesomeIcon icon={faTwitter} />
 <FontAwesomeIcon icon={faInstagram} />
+</div>
 
-<form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Phone</label>
-      <input type="email" name="user_phone" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-
-    </div>
+    
     );
 }
 
